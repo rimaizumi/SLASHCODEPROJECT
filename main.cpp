ac5a054679a57743af852e9ebb5f3e3c3b0fbcb2
HitlersFavoriteJew <luigiagas32@gmail.com>
2024-02-23 21:14:27 +0800

    commit2(new)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <raylib.h>
 using namespace std;
-Texture2D loading;
-Texture2D logo;
+
+
 typedef enum GAMESCREEN {Logo = 0, Title, Menu, Gameplay, Ending} GAMESCREEN;
 int main () {
 
@@ -12,77 +12,110 @@ int main () {
     InitWindow(screenWidth, screenHeight, "Project: Slash Code (ALPHA)");
     SetTargetFPS(60);
     GAMESCREEN currentScreen = Logo;
-
+     int framesCounter = 0;
     /**************************      USED FOR TITLE SCREEN ANIMATION        **************************************************/
-    loading = LoadTexture("resources/bg/Main.png");
-    int currentFrame = 0;
-    Rectangle frameRec = { 0.0f, 0.0f, (float)loading.width/15, (float)loading.height };
-    int framesCounter = 0;
-    int framesSpeed = 7;
+    
 
     /**************************      USED FOR LOGO SCREEN ANIMATION        **************************************************/            
-    logo = LoadTexture("resources/bg/Logot.png");
-    Rectangle frameRecLogo = { 0.0f, 0.0f, (float)logo.width/18, (float)logo.height };
+
    
    
     SetTargetFPS(60);
     while (!WindowShouldClose()) //checks if the user clicks the close button
     {   
         /********        This handles the switching of the screen           *******/
-        switch(currentScreen) 
-        {
-            case Logo: 
-            {
-                
-                framesCounter++;
-                if(GetTime() > 5.0f) {
-                    currentScreen = Title;
-                }
-            }break;
-
-            case Title: 
-            {
-
-            }
-        }
-        
-        //Draws all the important thing in the program
-
-
-        BeginDrawing();
-        ClearBackground(BLUE);
-        switch (currentScreen)
+        switch(currentScreen)
         {
             case Logo:
             {
-                if (framesCounter >= (60/framesSpeed))
+                // TODO: Update LOGO screen variables here!
+
+                framesCounter++;    // Count frames
+
+                // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+                if (framesCounter > 120)
                 {
-                    framesCounter = 0;
-                    currentFrame++;
-
-                    if (currentFrame > 14) currentFrame = 14;
-
-                    frameRecLogo.x = (float)currentFrame*(float)logo.width/18;
+                    currentScreen = Title;
                 }
+            } break;
+            case Title:
+            {
+                // TODO: Update TITLE screen variables here!
+
+                // Press enter to change to GAMEPLAY screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+                {
+                    currentScreen = Gameplay;
+                }
+            } break;
+            case Gameplay:
+            {
+                // TODO: Update GAMEPLAY screen variables here!
+
+                // Press enter to change to ENDING screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+                {
+                    currentScreen = Ending;
+                }
+            } break;
+            case Ending:
+            {
+                // TODO: Update ENDING screen variables here!
+
+                // Press enter to return to TITLE screen
+                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+                {
+                    currentScreen = Title;
+                }
+            } break;
+            default: break;
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            switch(currentScreen)
+            {
+                case Logo:
+                {
+                    // TODO: Draw LOGO screen here!
+                    DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
+                    DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
+
+                } break;
+                case Title:
+                {
+                    // TODO: Draw TITLE screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+                    DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+                    DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+                } break;
+                case Gameplay:
+                {
+                    // TODO: Draw GAMEPLAY screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+                    DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
+                    DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+
+                } break;
+                case Ending:
+                {
+                    // TODO: Draw ENDING screen here!
+                    DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+                    DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+                    DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+
+                } break;
+                default: break;
             }
-        }
-        
-        /*****************************TITLE*****************************/
-        if (framesCounter >= (60/framesSpeed))
-        {
-            framesCounter = 0;
-            currentFrame++;
 
-            if (currentFrame > 14) currentFrame = 14;
-
-            frameRec.x = (float)currentFrame*(float)loading.width/15;
-        }
-        /****************************************************************/
-        
-        DrawTextureRec(loading, frameRec, Vector2{0.0f, 0.0f}, WHITE); 
-        
-        
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
     CloseWindow();
     return 0;
